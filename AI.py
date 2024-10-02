7dad4388b6e931381cdedf33eaef8ee3bf158200
sachinmxgalaxy <businessforsac@gmail.com>
2024-10-01 20:58:31 -0400

    backend update to connect

--- AI.py
+++ AI.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="front_end")
     app.config['SECRET_KEY'] = 'alvin'
+    @app.route('/')
+##- HTML integration here
+    def hello_world():
+        return render_template('web.html')
+    
     return app
 
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
 
-##- HTML integration here
